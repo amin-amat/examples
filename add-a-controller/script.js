d495988732e02135f5006f0d1873fe0dc09e1e06
Amin Amat <aminamat@yahoo.com>
2015-09-15 22:45:46 -0700

    MadLibs Update
    Support Male and Female Names

--- add-a-controller/script.js
+++ add-a-controller/script.js
@@ -1,5 +1,19 @@
 angular.module('myApp', []).
 controller('madLibs', function($scope) {
-	[];
+	$scope.male =[
+		{
+		id: 'man',
+		noun: 'he',
+		possesiveNoun : 'his'
+		}
+		];
+	$scope.female =[
+		{
+		id: 'woman',
+		noun: 'she',
+		possesiveNoun : 'her'
+		}
+	];
+	
 	
 });
