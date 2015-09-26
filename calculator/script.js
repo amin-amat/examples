33052c77aaada99c4288bdc20c30712914e0184f
Amin Amat <aminamat@yahoo.com>
2015-09-25 21:55:14 -0700

    Calculator Update

--- calculator/script.js
+++ calculator/script.js
@@ -1,6 +1,47 @@
 angular.module('calcApp',[])
 .controller('calc', function($scope) {
 
+
+$scope.earnings = [];
+
+
+$scope.tipTotal = function() {
+	return ($scope.tipPercent / 100 ) * $scope.mealPrice;
+};
+
+$scope.tipMeal = function() {
+	return ($scope.tipEarningsTotal / $scope.mealCount);
+};
+
+$scope.mealCount = function() {
+	return ($scope.earnings.length);
+
+};
+
+$scope.update = function() {
+	var calculateTip = $scope.tipTotal();
+	$scope.earnings.push(calculateTip);
+}
+
+$scope.tipEarningsTotal = function() {
+	var sum = 0;
+	$scope.earnings.map(function(item) {
+		sum += item;
+	});
+	//console.log(sum);
+	return (sum);
+};
+
+$scope.tipMeal = function() {
+	return (($scope.tipEarningsTotal()) / $scope.earnings.length);
+};
+
+$scope.reset = function() {
+	$scope.earnings = [];
+	$scope.mealCount = 0;
+	tipEarningsTotal()
+
+};
 	
 })
 
